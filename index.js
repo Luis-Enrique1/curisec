89504a6e9cc50a67297c615f2956b98c223f486c
Luis-Enrique1 <piowanau@gmail.com>
2019-08-06 13:52:29 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.sombra', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.scroll', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.reveal', {
+    duration: 1000,
+    origin: 'bottom'
+});
+
+sr.reveal('.reveal2', {
+    duration: 2000,
+    origin: 'bottom'
+});
+
+sr.reveal('.reveal3', {
+    duration: 3000,
+    origin: 'bottom'
+});
